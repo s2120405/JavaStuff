f4c80674d89cf5495efed16846decd37359d1008
s2120405 <92307093+s2120405@users.noreply.github.com>
2022-11-16 20:15:00 +0800

    Delete Collection.java

--- Polymorph_Calampinay/Collection.java
+++
@@ -1,9 +0,0 @@
-package Polymorph_Calampinay;
-
- class Collection {
-
-
-    public void allCollection() {
-        System.out.println("You open up the rocks and found the following metals: ");
-    }
-}
